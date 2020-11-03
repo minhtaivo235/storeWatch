1f669e05ae8a3f49f9a65b7a7cb76711951aef8f
minhtai <minhtaivo235@gmail.com>
2020-11-04 00:06:57 +0700

    update animation header

--- js/main.js
+++ js/main.js
@@ -2,3 +2,43 @@ function nav(url) {
     var getUrl = url.getAttribute("data-nav");
     window.location.href = getUrl;
 }
+
+const header = document.querySelector('.header');
+const slider = document.querySelector('.slider');
+let statusChangeHeader = false;
+let scrollPos = 0;
+
+document.addEventListener("scroll", function () {
+    if (!statusChangeHeader) {
+        if (window.scrollY >= (slider.offsetTop + 300)) {
+            header.classList.add('posi-fixed');
+            statusChangeHeader = true;
+        }
+
+    }
+    else {
+        if (window.scrollY < (slider.offsetTop + 300)) {
+            header.classList.remove('posi-fixed');
+            statusChangeHeader = false;
+        }
+    }
+
+    // detects new state and compares it with the new one
+    if ((document.body.getBoundingClientRect()).top > scrollPos)
+        // document.querySelector('.header').setAttribute('data-animate', 'skewX(8deg) rotateX(5deg)');
+        document.querySelector('.header').style.transform = 'skewX(4deg) translateY(-0.2rem)';
+    else
+        // document.querySelector('.header').setAttribute('data-animate', 'skewX(-8deg) rotateX(-5deg)');
+        document.querySelector('.header').style.transform = 'skewX(-4deg) translateY(0.2rem)';
+    // saves the new position for iteration.
+    scrollPos = (document.body.getBoundingClientRect()).top;
+    setTimeout(function () {
+        document.querySelector('.header').style.transform = '';
+    }, 200);
+})
+
+var a = document.querySelector('.header');
+console.log(a)
+
+
+
