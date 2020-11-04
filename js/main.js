913cd287a6d42db9b24f74dd152bd41c0c0aacce
minhtai <minhtaivo235@gmail.com>
2020-11-04 22:17:06 +0700

    fix animation header

--- js/main.js
+++ js/main.js
@@ -8,37 +8,40 @@ const slider = document.querySelector('.slider');
 let statusChangeHeader = false;
 let scrollPos = 0;
 
-document.addEventListener("scroll", function () {
-    if (!statusChangeHeader) {
-        if (window.scrollY >= (slider.offsetTop + 300)) {
-            header.classList.add('posi-fixed');
-            statusChangeHeader = true;
+if (window.innerWidth > 1023) {
+
+    document.addEventListener("scroll", function () {
+        if (!statusChangeHeader) {
+            if (window.scrollY >= (slider.offsetTop + 300)) {
+                header.classList.add('posi-fixed');
+                statusChangeHeader = true;
+            }
+
         }
-
-    }
-    else {
-        if (window.scrollY < (slider.offsetTop + 300)) {
-            header.classList.remove('posi-fixed');
-            statusChangeHeader = false;
+        else {
+            if (window.scrollY < (slider.offsetTop + 300)) {
+                header.classList.remove('posi-fixed');
+                statusChangeHeader = false;
+            }
         }
-    }
+        // detects new state and compares it with the new one
+        if ((document.body.getBoundingClientRect()).top > scrollPos)
+            // document.querySelector('.header').setAttribute('data-animate', 'skewX(8deg) rotateX(5deg)');
+            document.querySelector('.header').style.transform = 'skewX(1deg) translateY(-0.2rem)';
+        else
+            // document.querySelector('.header').setAttribute('data-animate', 'skewX(-8deg) rotateX(-5deg)');
+            document.querySelector('.header').style.transform = 'skewX(-1deg) translateY(0.2rem)';
+        // saves the new position for iteration.
+        scrollPos = (document.body.getBoundingClientRect()).top;
+        setTimeout(function () {
+            document.querySelector('.header').style.transform = '';
+        }, 200);
+    })
+}
 
-    // detects new state and compares it with the new one
-    if ((document.body.getBoundingClientRect()).top > scrollPos)
-        // document.querySelector('.header').setAttribute('data-animate', 'skewX(8deg) rotateX(5deg)');
-        document.querySelector('.header').style.transform = 'skewX(4deg) translateY(-0.2rem)';
-    else
-        // document.querySelector('.header').setAttribute('data-animate', 'skewX(-8deg) rotateX(-5deg)');
-        document.querySelector('.header').style.transform = 'skewX(-4deg) translateY(0.2rem)';
-    // saves the new position for iteration.
-    scrollPos = (document.body.getBoundingClientRect()).top;
-    setTimeout(function () {
-        document.querySelector('.header').style.transform = '';
-    }, 200);
-})
 
-var a = document.querySelector('.header');
-console.log(a)
+
+
 
 
 
