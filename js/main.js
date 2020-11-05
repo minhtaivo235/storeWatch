9508635a08cb00dbce33deebca4b53f76a621ffa
minhtai <minhtaivo235@gmail.com>
2020-11-05 13:50:32 +0700

    update mobile header part 1

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const header = document.querySelector('.header');
 const slider = document.querySelector('.slider');
 let statusChangeHeader = false;
 let scrollPos = 0;
+let owlSearch = $(".search .owl-carousel");
 
 if (window.innerWidth > 1023) {
 
@@ -38,6 +39,40 @@ if (window.innerWidth > 1023) {
         }, 200);
     })
 }
+
+
+else {
+    owlSearch.owlCarousel({
+        items: 3,
+        loop: false,
+        nav: true,
+        dots: false
+    });
+
+    let header_bottom = document.querySelector('.header .header__bottom');
+
+
+    document.addEventListener("scroll", function () {
+        if (!statusChangeHeader) {
+            if (window.scrollY > 50) {
+                header_bottom.classList.add('d-none');
+                header.classList.add('fixed');
+                statusChangeHeader = true;
+            }
+
+        }
+        else {
+            if (window.scrollY <= 50) {
+                header_bottom.classList.remove('d-none');
+                header.classList.remove('fixed');
+                statusChangeHeader = false;
+            }
+        }
+    });
+}
+
+
+
 
 
 
