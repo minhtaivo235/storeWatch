bfb6ec0bfd2a49b4f748857653e804fee4f0a76a
minhtai <minhtaivo235@gmail.com>
2020-11-10 00:19:35 +0700

    completed

--- js/main.js
+++ js/main.js
@@ -54,10 +54,10 @@ window.addEventListener('DOMContentLoaded', function () {
             // detects new state and compares it with the new one
             if ((document.body.getBoundingClientRect()).top > scrollPos)
                 // document.querySelector('.header').setAttribute('data-animate', 'skewX(8deg) rotateX(5deg)');
-                document.querySelector('.header').style.transform = 'skewX(1deg) translateY(-0.2rem)';
+                document.querySelector('.header').style.transform = 'skewX(2deg) translateY(-0.2rem)';
             else
                 // document.querySelector('.header').setAttribute('data-animate', 'skewX(-8deg) rotateX(-5deg)');
-                document.querySelector('.header').style.transform = 'skewX(-1deg) translateY(0.2rem)';
+                document.querySelector('.header').style.transform = 'skewX(-2deg) translateY(0.2rem)';
             // saves the new position for iteration.
             scrollPos = (document.body.getBoundingClientRect()).top;
             setTimeout(function () {
